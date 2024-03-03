aad277061c1dbf5f45e64afa6e5e978a255832bf
Fernando Generato <generatof@gmail.com>
2024-03-03 20:25:48 -0300

    Add files via upload

---
+++ src/main/java/com/graphql/demo/BookController.java
@@ -0,0 +1,24 @@
+package com.graphql.demo;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @QueryMapping
+    public Book bookByAuthorID(@Argument String id) {
+        return Book.getByAuthorID(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
